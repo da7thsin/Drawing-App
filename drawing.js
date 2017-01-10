826a201f83b21c13cc6d6d5eb32de51985c7b3d4
Geoffrey <geo.galagaran@gmail.com>
2017-01-10 12:59:29 +0800

    Created canvas Module object

--- drawing.js
+++ drawing.js
@@ -1,5 +1,28 @@
 var canvas = (function(){
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
 
-});
+  ctx.canvas.width = 800;
+  ctx.canvas.height = 600;
+
+  function draw(callback){
+    callback(ctx);
+  }
+
+  function getCanvasDim(){
+    return {width: ctx.canvas.width, height: ctx.canvas.height};
+  }
+
+  function setCanvasDim(width, height){
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+  }
+
+  return{
+    getCanvasDim: getCanvasDim,
+    setCanvasDim: setCanvasDim,
+    draw: draw
+  }
+})();
 
 console.log(canvas);
